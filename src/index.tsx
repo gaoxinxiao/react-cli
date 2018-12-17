f444789b09e62b3f0030e6332ff51631ae99525d
高鑫效 <gaoxinxiao@gaoxinxiaodeMacBook-Pro.local>
2018-12-17 13:26:35 +0800

    class继承666

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,22 @@ import * as ReactDOM from 'react-dom'
 import App from './app'
 import '../node_modules/antd/dist/antd.min.css'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+
+class Parent{
+    public name = 'gxx'
+    public sex = '男'
+    private age = 123
+    getName(val?:any){
+        console.log(val)
+    }
+}
+
+class Son extends Parent{
+    constructor(props:any){
+        super()
+    }
+}
+
+new Son('wmh').getName()
+
+ReactDOM.render(<App />, document.getElementById('root'))
